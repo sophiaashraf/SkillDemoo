92ff888d5c90ed8be2e0d92be7a00d6b728cb8db
sophiaashraf <sashraf@ucsd.edu>
2022-05-01 18:30:57 -0700

    Added Tester

---
+++ SkillDemoTester.java
@@ -0,0 +1,10 @@
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class SkillDemoTester {
+    @Test
+    public void subtraction(){
+        assertEquals(5, SkillDemoo.sub(10,3));
+    }
+    
+}
--- SkillDemoo.java
+++ SkillDemoo.java
@@ -1,2 +1,5 @@
 public class SkillDemoo{
+    public static int sub(int a, int b){
+        return a - b;
+    }
 }
